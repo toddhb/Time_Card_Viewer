cd7af7dff28e9645215182d331b7f7a2f9c56454
Cameron White <cameron.white@jivesoftware.com>
2015-05-25 23:22:02 -0700

    Refactored Calendar component

--- app/scripts/ui/Calendar.js
+++ app/scripts/ui/Calendar.js
@@ -21,47 +21,42 @@ var Calendar = React.createClass({
       ].map(function(each) {
         return (<th>{each}</th>)
       });
-    var days = _.range(1, this.dayOfDate(+this.props.year, +this.props.month, 1)+1)
-        .map(function(each) { 
-            return (<td></td>)
-        }).concat( 
-          _.range(1, (this.daysInMonth(+this.props.year, +this.props.month)+1))
-            .map(function(each) {
-              return (<td>{each}</td>)
-            })
-        );
-    var rows = _.reduce(days, function(memo, it){ 
-        var last = _.last(memo)
-        if (last.length < 7) {
-          last.push(it)
-        } else {
-          memo.push([it])  
-        }
-        return memo; 
-      }, [[]]).map(function (each) {
-        return <tr className="week">{each}</tr>
-      });
-    return (
-        <div className="container-fluid">
-        <div className="calendar">
-            <div className="row month-title">
-                <div className="col-xs-12">
-                    <h1>{this.props.month}</h1>
-                </div>
-            </div>
-            
-            <div className="row calendar-row">
-                <div className="col-xs-12">
-                    <table width="100%">
-                    {headers}    
-                    {rows}
-                    </table>
-                </div>
-                </div>
-            </div>
-        </div>
+
+    var day = this.dayOfDate(+this.props.year, +this.props.month, 1)
+    var daysInMonth = this.daysInMonth(+this.props.year, +this.props.month, 1)
+
+    var days = _.range(day+1).map(function(each) {
+        return (<td></td>)
+      }).concat(
+        _.range(1, (daysInMonth+1))
+         .map(function(each) { return (<td>{each}</td>) })
       );
-    }
+
+    var rows = _.chain(days)
+      .groupBy(function(element, index){ return Math.floor(index/7); })
+      .map(function(eachWeek) { return <tr className="week">{eachWeek}</tr>})
+
+    return (
+      <div className="container-fluid">
+        <div className="calendar">
+          <div className="row month-title">
+            <div className="col-xs-12">
+              <h1>{this.props.month}</h1>
+            </div>
+          </div>
+
+          <div className="row calendar-row">
+            <div className="col-xs-12">
+              <table width="100%">
+                {headers}
+                {rows}
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 });
     
 module.exports = Calendar
