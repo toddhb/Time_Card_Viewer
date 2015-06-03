456ffd1f9851ffd83a646e911a9a1400b689ebca
Cameron White <cameron.white@jivesoftware.com>
2015-06-03 16:50:21 -0700

    Added a experimental CountDown Component

---
+++ app/scripts/ui/CountDown.js
@@ -0,0 +1,43 @@
+exports class Timer extends React.Component {
+  getInitialState() {
+    return {
+      now: new Date(Date.now())
+    }
+  },
+  updateDate() {
+      this.setState({
+          now: new Date(Date.now())
+      })
+  },
+  componentDidMount() {
+      this.updateDate()
+      setInterval(this.updateDate, 100);
+  },
+  render() {
+    var now = this.state.now
+    var end = this.props.end
+    var hours = minutes = secounds = days = 0
+    if (end > now) {
+      hours = (end.getHours()-now.getHours())%24 - 1
+      minutes = 60 + (60*end.getMinutes()-now.getMinutes())%60 - 1 
+      secounds = 60 + (60*end.getSeconds()-now.getSeconds())%60 - 1 
+      days = Math.floor((end-now)/86400000) 
+    }
+    return (
+      <table>
+        <tr>
+          <th>Days</th>
+          <th>Hours</th>
+          <th>Mins</th>
+          <th>Seconds</th>
+        </tr>
+        <tr>
+          <td>{days}</td>
+          <td>{hours}</td>
+          <td>{minutes}</td>
+          <td>{secounds}</td>
+        </tr>
+      </table>
+    )
+  }
+}
