a3c88346f28b5d2086bfc539a50e3363f5cc46cf
Sasha Fahrenkopf <sfahrenk@gmail.com>
2015-06-22 16:24:20 -0700

    Added full-width header

--- app/scripts/ui/Main.js
+++ app/scripts/ui/Main.js
@@ -2,14 +2,32 @@ import React from 'react'
 import Router, { RouteHandler, Link} from "react-router"
 import Navbar from "./Navbar.js"
 import AlertBar from "./AlertBar.js"
+    
+class NavContainer extends React.Component {
+  render() {
+    return (
+      <Navbar title="Steve's Time" />
+    ) 
+  } 
+}
+
+class BodyContainer extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <AlertBar />
+        <RouteHandler {...this.params} />
+      </div>
+    ) 
+  } 
+}
 
 export default class Main extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Navbar title="Steve's Time Card" />
-        <AlertBar />
-        <RouteHandler {...this.params} />
+      <div>
+        <NavContainer></NavContainer>
+        <BodyContainer></BodyContainer>
       </div>
     ) 
   } 
--- app/scripts/ui/Navbar.js
+++ app/scripts/ui/Navbar.js
@@ -4,11 +4,13 @@ import Router, { RouteHandler, Link} from "react-router"
 export default class Navbar {
   render() {
     return (
-      <div className="header">
-        <h3 className="text-muted">
-          <Link to="/">{this.props.title}</Link>
-        </h3>
-      </div>
+        <div className="container-fluid">
+          <div className="header">
+            <h3 className="text-muted">
+              <Link to="/">{this.props.title}</Link>
+            </h3>
+          </div>
+        </div>
     )
   }
 }
