31c135b7f67ad4914c87b26bb781341d9e71007b
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-06-24 14:57:32 -0700

    Added CalendarOverview template component and route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,6 +6,7 @@ import { Provider } from "redux/react"
 import * as stores from "./stores"
 import DayOverview from "./ui/DayOverview"
 import PayPeriodsOverview from "./ui/PayPeriodsOverview"
+import CalendarOverview from "./ui/CalendarOverview"
 import Main from "./ui/Main.js"
 import FluxComponent from 'flummox/component';
 import flux from "./stores/flux"
@@ -26,6 +27,7 @@ const routes = (
   <Route name="app" handler={App} path="/">
     <DefaultRoute handler={PayPeriodsOverview} />
     <Route name="day" path="day/:date" handler={DayOverview} />
+    <Route name="calendar" handler={CalendarOverview} />
   </Route>
 )
 
---
+++ app/scripts/ui/CalendarOverview.js
@@ -0,0 +1,9 @@
+import React from 'react'
+    
+export default class CalendarOverview extends React.Component {
+  render() {
+    return (
+      <h1>CalendarOverview - Not Implemented</h1>
+    ) 
+  }
+}
