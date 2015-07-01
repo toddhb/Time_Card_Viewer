293ed71ec73d0035631a0ace7dcbbbc43140b204
toddhb <tbrochu@pdx.edu>
2015-06-30 21:22:29 -0700

    TCV-100 Add copyright notices to all the files

--- app/scripts/stores/flux.js
+++ app/scripts/stores/flux.js
@@ -1,3 +1,11 @@
+/*
+ * TimeCard View
+ * Copyright ©2015 Thomas Nelson, Jacob Nichols, David Opp, Todd Brochu,
+Andrew McGown, Sasha Fahrenkopf, Cameron B. White.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE text file in the root directory of this source tree.
+ */
 import { Flummox } from 'flummox';
 import { DayActions, DayStore } from "./days"
 import { DayStreamActions, DayStreamStore} from "./daystream"
