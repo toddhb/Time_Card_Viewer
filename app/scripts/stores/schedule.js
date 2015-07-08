6888582a40f38faf2ac3141515ecd0b73f62d0eb
David Opp <dopp@pdx.edu>
2015-07-07 18:05:26 -0700

    TCV-96 Added static json object in schedule store

--- app/scripts/stores/schedule.js
+++ app/scripts/stores/schedule.js
@@ -29,5 +29,531 @@ export class ScheduleStore extends Store {
 }
 
 const defaultState = {
-   Schedule: {}
+  "Schedule": {
+    "-QueryDateSpan": "4/01/2015 - 4/30/2015",
+    "Employees": {
+      "PersonIdentity": { "-PersonNumber": "N0686" }
+    },
+    "ScheduleItems": {
+      "ScheduleShift": [
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/29/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/29/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/29/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/28/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/28/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/28/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/27/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/27/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/27/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/30/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/30/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/30/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/24/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/24/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/24/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/23/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/23/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/23/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/21/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/21/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/21/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/20/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/20/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/20/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/17/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/17/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/17/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/16/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/16/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/16/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/15/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/15/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/15/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/14/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/14/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/14/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/13/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/13/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/13/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/10/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/10/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/10/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/09/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/09/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/09/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/08/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/08/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/08/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/06/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/06/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/06/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/07/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/07/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/07/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/02/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/02/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/02/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/03/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/03/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/03/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/22/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/22/2015",
+              "-StartTime": "8:30",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/22/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        },
+        {
+          "-LockedFlag": "false",
+          "-StartDate": "4/01/2015",
+          "-IsDeleted": "false",
+          "Employee": {
+            "PersonIdentity": { "-PersonNumber": "N0686" }
+          },
+          "ShiftSegments": {
+            "ShiftSegment": {
+              "-SegmentTypeName": "REGULAR",
+              "-StartDate": "4/01/2015",
+              "-StartTime": "8:00",
+              "-StartDayNumber": "1",
+              "-EndDate": "4/01/2015",
+              "-EndTime": "16:30",
+              "-EndDayNumber": "1"
+            }
+          }
+        }
+      ]
+    },
+    "SchedulePatterns": {
+      "SchedulePattern": {
+        "-AnchorDate": "8/03/2014",
+        "-SchedulePatternName": " #257213",
+        "-EffectiveDateSpan": "8/03/2014 - 1/01/3000",
+        "-PeriodLengthInDays": "7",
+        "-PeriodLength": "1",
+        "-PeriodCodeName": "WEEKS",
+        "-IsTemplateFlag": "false",
+        "-IsOverrideFlag": "false",
+        "Employee": {
+          "PersonIdentity": { "-PersonNumber": "N0686" }
+        },
+        "PatternElements": {
+          "PatternElement": [
+            {
+              "-DayNumber": "2",
+              "ShiftCode": {
+                "ShiftCode": {
+                  "ShiftSegments": {
+                    "ShiftSegment": {
+                      "-SegmentTypeName": "REGULAR",
+                      "-StartTime": "8:00",
+                      "-StartDayNumber": "1",
+                      "-EndTime": "16:30",
+                      "-EndDayNumber": "1"
+                    }
+                  }
+                }
+              }
+            },
+            {
+              "-DayNumber": "3",
+              "ShiftCode": {
+                "ShiftCode": {
+                  "ShiftSegments": {
+                    "ShiftSegment": {
+                      "-SegmentTypeName": "REGULAR",
+                      "-StartTime": "8:00",
+                      "-StartDayNumber": "1",
+                      "-EndTime": "16:30",
+                      "-EndDayNumber": "1"
+                    }
+                  }
+                }
+              }
+            },
+            {
+              "-DayNumber": "4",
+              "ShiftCode": {
+                "ShiftCode": {
+                  "ShiftSegments": {
+                    "ShiftSegment": {
+                      "-SegmentTypeName": "REGULAR",
+                      "-StartTime": "8:00",
+                      "-StartDayNumber": "1",
+                      "-EndTime": "16:30",
+                      "-EndDayNumber": "1"
+                    }
+                  }
+                }
+              }
+            },
+            {
+              "-DayNumber": "5",
+              "ShiftCode": {
+                "ShiftCode": {
+                  "ShiftSegments": {
+                    "ShiftSegment": {
+                      "-SegmentTypeName": "REGULAR",
+                      "-StartTime": "8:00",
+                      "-StartDayNumber": "1",
+                      "-EndTime": "16:30",
+                      "-EndDayNumber": "1"
+                    }
+                  }
+                }
+              }
+            },
+            {
+              "-DayNumber": "6",
+              "ShiftCode": {
+                "ShiftCode": {
+                  "ShiftSegments": {
+                    "ShiftSegment": {
+                      "-SegmentTypeName": "REGULAR",
+                      "-StartTime": "8:00",
+                      "-StartDayNumber": "1",
+                      "-EndTime": "16:30",
+                      "-EndDayNumber": "1"
+                    }
+                  }
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+  }
 }
