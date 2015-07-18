12aad293f51961fa1b0d7013112218e839e780bb
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-07-18 12:24:52 -0700

    Cleaned up and simplified Day component

--- app/scripts/components/PayPeriod/PayPeriod.js
+++ app/scripts/components/PayPeriod/PayPeriod.js
@@ -98,45 +98,19 @@ class Day extends React.Component {
   // of hours worked that day. Both date and hours are 
   // passed through props.
   render() {
-    var date = this.props.Date.split('/').map(part =>parseInt(part))
-    date = moment().set({'M': (date[0] - 1), 
-                         'D': date[1],
-                         'Y': date[2]}) 
-    var timeDisplay = ""
-    var hoursWorked = this.props.GrandTotal
-
-    //a day off from work
-    if(hoursWorked === undefined){
-      timeDisplay = "0"
-    }
-    //a workday
-    else {
-      const hours = hoursWorked.split(":")[0]
-      var minutes = hoursWorked.split(":")[1]
-
-      //convert minutes from
-      //time xx:xx to decimal xx.xx
-      minutes = Math.round((minutes*1)*(5/3))
-
-      if(minutes === 0) {
-        timeDisplay = hours
-      }
-      else {
-        timeDisplay = hours + "." + minutes
-      }
-    }
+    const { GrandTotal } = this.props
+    const date = moment(this.props.Date, 'M/DD/YYYY')
+    const hoursWorked = GrandTotal ? Number(GrandTotal.replace(':', '.')) : 0
     return (
       <li className="day-as-txt">
         <div className="time-entry shadowed-box">
-      {<Link to="day" params={{ date: date.format("YYYY-MM-DD")}}>
+          <Link to="day" params={{ date: date.format("YYYY-MM-DD")}}>
             <div className="date-side-box">
                 <p className="day-as-text text-center">{date.format("dddd")}</p>
                 <p className="date text-center"><span className="day-as-number">{date.format("M/D")}</span></p>
             </div>
-                {<p className="hours-worked-text"><span className="hours-worked-number text-center">{timeDisplay}</span>
-            hours worked</p>}
+            <p className="hours-worked-text"><span className="hours-worked-number text-center">{hoursWorked}</span> hours worked</p>
           </Link>
-        }
         </div>
       </li>
     )
