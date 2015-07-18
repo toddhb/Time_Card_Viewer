ac754532eca47cbf1b638b14926abde5a33dbeb6
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-07-18 14:04:07 -0700

    Removed HoursPerCode

--- app/scripts/components/PeriodStats/PeriodStats.js
+++ app/scripts/components/PeriodStats/PeriodStats.js
@@ -66,9 +66,6 @@ export default class PeriodStats extends React.Component {
             <p className="text-right"><strong>${allOvertime.AmountInCurrency}</strong></p>
           </div>
         </div>
-        <div className="col-md-4 hidden-xs">
-          <HoursPerCode />
-        </div>
         <div className="col-md-2 hidden-xs">
           <FluxComponent connectToStores={['currentPeriod']}>
             <LatePunches />
@@ -121,18 +118,6 @@ class LatePunches extends React.Component {
       <div>
         <p className="text-center"><strong>{timesLate}</strong></p>
         <h4 className="text-center">Late Punches</h4>
-      </div>
-    );
-  }
-}
-
-class HoursPerCode extends React.Component {
-  // Returns the hours break down per each code in the pay period
-  render() {
-    return (
-      <div>
-        <h4 style={{marginTop:0 + "px"}}>Hours per clock code</h4>
-        <p>Needs implementing</p>
       </div>
     );
   }
